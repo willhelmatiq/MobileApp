4dc6233f1614d46142a78b4fddb8d8bf23feb0b8
Willhelmatiq <rot3ad@gmail.com>
2023-07-17 09:25:56 +0200

    hw finished

--- app/src/main/java/com/harbourspace/myapplication/MainActivity.kt
+++ app/src/main/java/com/harbourspace/myapplication/MainActivity.kt
@@ -1,15 +1,25 @@
 package com.harbourspace.myapplication
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.harbourspace.myapplication.ui.theme.MobileAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +32,18 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android 123")
+                    Column(
+                        modifier = Modifier.fillMaxWidth(),
+                        verticalArrangement = Arrangement.Center
+                    ) {
+
+                        AddItem(
+                            resId = R.string.main_details,
+                            action = { startActivity(Intent(applicationContext, DetailsActivity::class.java)) }
+                        )
+
+
+                    }
                 }
             }
         }
@@ -42,5 +63,21 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 fun GreetingPreview() {
     MobileAppTheme {
         Greeting("Android")
+    }
+}
+
+@Composable
+fun AddItem(
+    @StringRes resId: Int,
+    action: () -> Unit
+
+) {
+    Spacer(modifier = Modifier.height(16.dp))
+
+    Button(
+        modifier = Modifier.fillMaxWidth(),
+        onClick = { action() }
+    ) {
+        Text(stringResource(id = resId))
     }
 }
